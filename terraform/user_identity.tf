1ff4b238261cd193198cf91a9dffe3022412fda0
mishal <mishalshah92@gmail.com>
2021-03-29 23:16:00 +0530

    Typo fix

--- terraform/user_identity.tf
+++ terraform/user_identity.tf
@@ -3,5 +3,5 @@
 resource "azurerm_role_assignment" "app_gateway_user_identity_keyvault_admin" {
   scope                = azurerm_key_vault.rg_keyvault.id
   role_definition_name = "Key Vault Administrator"
-  principal_id         = azurerm_api_management.api_mgmt.identity["principal_id"]
+  principal_id         = azurerm_api_management.api_mgmt.identity[0].principal_id
 }
