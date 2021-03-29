188cc63322e00e6f851fe8d0a5bc35f36ec76b9f
Patel, VijayKumar <vpatel2@marvell.com>
2021-03-29 23:53:21 +0530

    updated output

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,7 @@
 output "certificate_data" {
-  value = azurerm_key_vault_certificate.api_mgmt_cert.certificate_data
+  value = azurerm_key_vault_certificate.api_mgmt_cert.certificate_data_base64
+}
+
+output "thumbprint" {
+  value = azurerm_key_vault_certificate.api_mgmt_cert.thumbprint
 }
